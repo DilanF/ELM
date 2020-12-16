ed873e863e6227f788bfdc906f526c6305c0a6f9
DilanF <54943178+DilanF@users.noreply.github.com>
2020-12-16 19:38:14 -0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,7 +22,7 @@ $('a[href^="#"]').on('click', function (event) {
   if (target.length) {
     event.preventDefault();
     $('html, body').animate({
-      scrollTop: target.offset().top - 50
+      scrollTop: target.offset().top - 70
     }, 500);
   }
 
